091f060c4323a717d5b87223021e8ff06a8c13f8
Zacharey Carey <zac4ncstate@aol.com>
2019-09-24 18:08:20 -0400

    first push

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./batsignal.jpg";
+import wrapper from "./src/wrapper";
+import cards from "./src/cards";
+import header from "./src/header";
 import "./App.css";
+
+class App extends Component {
+  state = {
+    cards,
+    PointsScored: 0,
+    highscore: 0
+  };
+}
+
+
+gameOver = () => {
+  if(this.state.PointsScored > this.state.highscore) {
+    this.setState({highscore: this.state.score}, function() {
+      console.log("highscore");
+    });
+  }
+
+}
+
+onclickCount = id => {
+  this.state.cards.find((o,i)) => {
+    if
+  }
+}
 
 var resetAndStart = function() {
   $(".images").empty();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={batsignal.jpg} className="App-logo" alt="logo" />
-        <p></p>
-        <a
-          className="App-link"
-          href="https://https://www.dccomics.com/characters/batman"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Batman Clicky Game
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+  function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={batsignal.jpg} className="App-logo" alt="logo" />
+          <p></p>
+          <a
+            className="App-link"
+            href="https://https://www.dccomics.com/characters/batman"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Batman Clicky Game
+          </a>
+        </header>
+      </div>
+    );
+  }
 
 $(".images").on("click", ".image", function() {
   var num = parseInt($(this).attr("data-random"));
